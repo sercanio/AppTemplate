05dabcadc819705e5524ba734770a7e65e42dba7
sercanio <104576153+sercanio@users.noreply.github.com>
2025-06-19 08:17:06 +0300

    feat: add StatisticsRead permission and Notifications table
    - Updated the `Permission` class to include `StatisticsRead` permission and added a constructor for initialization.
    - Modified `PermissionConfiguration` to register the new permission.
    - Created migration `20250619051201_addStatisticsPermissions` to define a new `Notifications` table with relevant fields.
    - Updated existing data in `AppUsers`, `AspNetUsers`, `Permissions`, and `Roles` tables to align with the new schema.
    - Updated `ApplicationDbContextModelSnapshot` to reflect the new database state.
    - Established a one-to-many relationship between `AppUser` and `Notification`.
    - Updated `RolePermission` and `RoleUser` join tables to include the new permission.

--- src/AppTemplate.Infrastructure/Migrations/20250619051201_addStatisticsPermissions.Designer.cs
+++ src/AppTemplate.Infrastructure/Migrations/20250619051201_addStatisticsPermissions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AppTemplate.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AppTemplate.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250619051201_addStatisticsPermissions")]
+    partial class addStatisticsPermissions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
